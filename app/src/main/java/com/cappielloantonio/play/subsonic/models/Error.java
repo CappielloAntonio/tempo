1a9e945790100e8a204ccb095a2e343f4ca41b6e
CappielloAntonio <antoniocappiello.play@gmail.com>
2021-07-25 14:43:48 +0200

    Removed comment

--- app/src/main/java/com/cappielloantonio/play/subsonic/models/Error.java
+++ app/src/main/java/com/cappielloantonio/play/subsonic/models/Error.java
@@ -1,45 +1,28 @@
 package com.cappielloantonio.play.subsonic.models;
 
+import com.cappielloantonio.play.subsonic.utils.converter.ErrorCodeConverter;
+import com.tickaroo.tikxml.annotation.Attribute;
+import com.tickaroo.tikxml.annotation.Xml;
+
+@Xml
 public class Error {
+    @Attribute(converter = ErrorCodeConverter.class)
     protected ErrorCode code;
+    @Attribute
     protected String message;
 
-    /**
-     * Gets the value of the code property.
-     *
-     * @return possible object is
-     * {@link ErrorCode }
-     */
     public ErrorCode getCode() {
         return code;
     }
 
-    /**
-     * Sets the value of the code property.
-     *
-     * @param value allowed object is
-     *              {@link ErrorCode }
-     */
     public void setCode(ErrorCode value) {
         this.code = value;
     }
 
-    /**
-     * Gets the value of the message property.
-     *
-     * @return possible object is
-     * {@link String }
-     */
     public String getMessage() {
         return message;
     }
 
-    /**
-     * Sets the value of the message property.
-     *
-     * @param value allowed object is
-     *              {@link String }
-     */
     public void setMessage(String value) {
         this.message = value;
     }

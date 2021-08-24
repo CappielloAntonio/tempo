3778202fa5a1d2c14e1838bb397f6ee22cb35159
CappielloAntonio <antoniocappiello.play@gmail.com>
2021-08-24 13:18:17 +0200

    Code refactor in ServerRepository

--- app/src/main/java/com/cappielloantonio/play/repository/ServerRepository.java
+++ app/src/main/java/com/cappielloantonio/play/repository/ServerRepository.java
@@ -5,18 +5,15 @@ import android.app.Application;
 import androidx.lifecycle.LiveData;
 
 import com.cappielloantonio.play.database.AppDatabase;
-import com.cappielloantonio.play.database.dao.QueueDao;
 import com.cappielloantonio.play.database.dao.ServerDao;
 import com.cappielloantonio.play.model.Server;
-import com.cappielloantonio.play.model.Song;
-import com.cappielloantonio.play.util.QueueUtil;
 
 import java.util.List;
 
 public class ServerRepository {
     private static final String TAG = "QueueRepository";
 
-    private ServerDao serverDao;
+    private final ServerDao serverDao;
 
     public ServerRepository(Application application) {
         AppDatabase database = AppDatabase.getInstance(application);
@@ -40,8 +37,8 @@ public class ServerRepository {
     }
 
     private static class InsertThreadSafe implements Runnable {
-        private ServerDao serverDao;
-        private Server server;
+        private final ServerDao serverDao;
+        private final Server server;
 
         public InsertThreadSafe(ServerDao serverDao, Server server) {
             this.serverDao = serverDao;
@@ -55,8 +52,8 @@ public class ServerRepository {
     }
 
     private static class DeleteThreadSafe implements Runnable {
-        private ServerDao serverDao;
-        private Server server;
+        private final ServerDao serverDao;
+        private final Server server;
 
         public DeleteThreadSafe(ServerDao serverDao, Server server) {
             this.serverDao = serverDao;

1dac0afa6f3b5dc0a6d33575d7b72ac7c42153f1
CappielloAntonio <antoniocappiello.play@gmail.com>
2021-08-24 13:12:54 +0200

    Reinforced response checks on ScanRepository

--- app/src/main/java/com/cappielloantonio/play/repository/ScanRepository.java
+++ app/src/main/java/com/cappielloantonio/play/repository/ScanRepository.java
@@ -2,10 +2,10 @@ package com.cappielloantonio.play.repository;
 
 import android.app.Application;
 
+import androidx.annotation.NonNull;
+
 import com.cappielloantonio.play.App;
 import com.cappielloantonio.play.interfaces.ScanCallback;
-import com.cappielloantonio.play.interfaces.SystemCallback;
-import com.cappielloantonio.play.subsonic.models.ResponseStatus;
 import com.cappielloantonio.play.subsonic.models.SubsonicResponse;
 
 import retrofit2.Call;
@@ -14,7 +14,7 @@ import retrofit2.Callback;
 public class ScanRepository {
     private static final String TAG = "SongRepository";
 
-    private Application application;
+    private final Application application;
 
     public ScanRepository(Application application) {
         this.application = application;
@@ -26,14 +26,14 @@ public class ScanRepository {
                 .startScan()
                 .enqueue(new Callback<SubsonicResponse>() {
                     @Override
-                    public void onResponse(Call<SubsonicResponse> call, retrofit2.Response<SubsonicResponse> response) {
-                        if (response.body().getScanStatus() != null) {
+                    public void onResponse(@NonNull Call<SubsonicResponse> call, @NonNull retrofit2.Response<SubsonicResponse> response) {
+                        if (response.isSuccessful() && response.body() != null && response.body().getScanStatus() != null) {
                             callback.onSuccess(response.body().getScanStatus().isScanning(), response.body().getScanStatus().getCount());
                         }
                     }
 
                     @Override
-                    public void onFailure(Call<SubsonicResponse> call, Throwable t) {
+                    public void onFailure(@NonNull Call<SubsonicResponse> call, @NonNull Throwable t) {
                         callback.onError(new Exception(t.getMessage()));
                     }
                 });
@@ -45,14 +45,14 @@ public class ScanRepository {
                 .startScan()
                 .enqueue(new Callback<SubsonicResponse>() {
                     @Override
-                    public void onResponse(Call<SubsonicResponse> call, retrofit2.Response<SubsonicResponse> response) {
-                        if (response.body().getScanStatus() != null) {
+                    public void onResponse(@NonNull Call<SubsonicResponse> call, @NonNull retrofit2.Response<SubsonicResponse> response) {
+                        if (response.isSuccessful() && response.body() != null && response.body().getScanStatus() != null) {
                             callback.onSuccess(response.body().getScanStatus().isScanning(), response.body().getScanStatus().getCount());
                         }
                     }
 
                     @Override
-                    public void onFailure(Call<SubsonicResponse> call, Throwable t) {
+                    public void onFailure(@NonNull Call<SubsonicResponse> call, @NonNull Throwable t) {
                         callback.onError(new Exception(t.getMessage()));
                     }
                 });

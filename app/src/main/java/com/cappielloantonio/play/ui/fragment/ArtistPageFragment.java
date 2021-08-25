a1673cd9c25e8c5901b626d6ce5ae648c7dc57f4
CappielloAntonio <antoniocappiello.play@gmail.com>
2021-08-25 17:46:13 +0200

    Revert last commit

--- app/src/main/java/com/cappielloantonio/play/ui/fragment/ArtistPageFragment.java
+++ app/src/main/java/com/cappielloantonio/play/ui/fragment/ArtistPageFragment.java
@@ -118,7 +118,7 @@ public class ArtistPageFragment extends Fragment {
             if (bind != null) bind.bioMoreTextViewClickable.setVisibility(artist.getLastfm() != null ? View.VISIBLE : View.GONE);
 
             CustomGlideRequest.Builder
-                    .from(requireContext(), artistPageViewModel.getArtist().getId(), CustomGlideRequest.ARTIST_PIC, /*artist.getImageUrl()*/ null)
+                    .from(requireContext(), /*artistPageViewModel.getArtist().getId()*/ null, CustomGlideRequest.ARTIST_PIC, artist.getImageUrl())
                     .build()
                     .into(bind.artistBackdropImageView);
 

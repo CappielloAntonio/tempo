695f406bd6b1b48412f0e1f0bd4e66df8966f947
CappielloAntonio <antoniocappiello.play@gmail.com>
2021-09-12 12:00:27 +0200

    Check network connection type before choosing the default bitrate

--- app/src/main/java/com/cappielloantonio/play/util/MusicUtil.java
+++ app/src/main/java/com/cappielloantonio/play/util/MusicUtil.java
@@ -1,6 +1,8 @@
 package com.cappielloantonio.play.util;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.text.Html;
 import android.util.Log;
 
@@ -30,8 +32,8 @@ public class MusicUtil {
                 "&v=" + params.get("v") +
                 "&c=" + params.get("c") +
                 "&id=" + song.getId() +
-                "&maxBitRate=" + PreferenceUtil.getInstance(context).getMaxBitrateWifi() +
-                "&format=" + PreferenceUtil.getInstance(context).getAudioTranscodeFormat();
+                "&maxBitRate=" + getBitratePreference(context) +
+                "&format=" + getFormatPreference(context);
     }
 
     public static MediaItem getSongDownloadItem(Song song) {
@@ -127,5 +129,22 @@ public class MusicUtil {
             default:
                 return R.drawable.default_album_art;
         }
+    }
+
+    private static String getBitratePreference(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        switch (connectivityManager.getActiveNetworkInfo().getType()) {
+            case ConnectivityManager.TYPE_WIFI:
+                return PreferenceUtil.getInstance(context).getMaxBitrateWifi();
+            case ConnectivityManager.TYPE_MOBILE:
+                return PreferenceUtil.getInstance(context).getMaxBitrateMobile();
+            default:
+                return PreferenceUtil.getInstance(context).getMaxBitrateWifi();
+        }
+    }
+
+    private static String getFormatPreference(Context context) {
+        return PreferenceUtil.getInstance(context).getAudioTranscodeFormat();
     }
 }

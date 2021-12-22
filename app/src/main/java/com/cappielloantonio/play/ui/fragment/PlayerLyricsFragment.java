0456edb1cc4719d3916c82cb05fd4cd105fddea8
CappielloAntonio <antoniocappiello.play@gmail.com>
2021-12-22 16:57:13 +0100

    Code cleanup

--- app/src/main/java/com/cappielloantonio/play/ui/fragment/PlayerLyricsFragment.java
+++ app/src/main/java/com/cappielloantonio/play/ui/fragment/PlayerLyricsFragment.java
@@ -1,8 +1,6 @@
 package com.cappielloantonio.play.ui.fragment;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;

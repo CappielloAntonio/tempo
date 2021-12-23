049621e9d674d367631c002451863a2c5c46b049
CappielloAntonio <antoniocappiello.play@gmail.com>
2021-12-23 17:10:03 +0100

    Changed client name

--- app/src/main/java/com/cappielloantonio/play/subsonic/SubsonicPreferences.java
+++ app/src/main/java/com/cappielloantonio/play/subsonic/SubsonicPreferences.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 public class SubsonicPreferences {
     private String serverUrl;
     private String username;
-    private String clientName = "Play for Subsonic";
+    private String clientName = "Play";
     private SubsonicAuthentication authentication;
 
     public String getServerUrl() {

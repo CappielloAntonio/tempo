c3b948808cb4b051ee23dabf5a1bdd13987005f7
CappielloAntonio <antoniocappiello.play@gmail.com>
2022-01-13 11:04:56 +0100

    Limited number of songs to shuffle

--- app/src/main/java/com/cappielloantonio/play/ui/fragment/SongListPageFragment.java
+++ app/src/main/java/com/cappielloantonio/play/ui/fragment/SongListPageFragment.java
@@ -152,7 +152,7 @@ public class SongListPageFragment extends Fragment {
         songListPageViewModel.getSongList(requireActivity()).observe(requireActivity(), songs -> {
             if (bind != null) {
                 bind.songListShuffleImageView.setOnClickListener(v -> {
-                    Collections.shuffle(songs);
+                    Collections.shuffle(songs.subList(0, Math.min(25, songs.size())));
                     MediaManager.startQueue(mediaBrowserListenableFuture, requireContext(), songs, 0);
                     activity.setBottomSheetInPeek(true);
                 });

b9ed67fedf72a8a8b48049175997c30d063db13e
antonio <antoniocappiello.play@gmail.com>
2022-03-25 22:28:54 +0100

    Added information on bitrate and file extension

--- app/src/main/java/com/cappielloantonio/play/subsonic/models/PodcastEpisode.java
+++ app/src/main/java/com/cappielloantonio/play/subsonic/models/PodcastEpisode.java
@@ -1,11 +1,9 @@
 package com.cappielloantonio.play.subsonic.models;
 
 import com.tickaroo.tikxml.annotation.Attribute;
-import com.tickaroo.tikxml.annotation.Element;
 import com.tickaroo.tikxml.annotation.Xml;
 import com.tickaroo.tikxml.converters.date.rfc3339.DateRfc3339TypeConverter;
 
-import java.time.LocalDateTime;
 import java.util.Date;
 
 @Xml

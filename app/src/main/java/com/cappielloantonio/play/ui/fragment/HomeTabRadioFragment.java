9f5e0a0e008b24b164cbf04b0865156858e888c6
antonio <antoniocappiello.play@gmail.com>
2023-05-06 19:27:14 +0200

    feat: split the home into three tab sub-sections

---
+++ app/src/main/java/com/cappielloantonio/play/ui/fragment/HomeTabRadioFragment.java
@@ -0,0 +1,72 @@
+package com.cappielloantonio.play.ui.fragment;
+
+import android.content.ComponentName;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.media3.common.util.UnstableApi;
+import androidx.media3.session.MediaBrowser;
+import androidx.media3.session.SessionToken;
+
+import com.cappielloantonio.play.databinding.FragmentHomeTabPodcastBinding;
+import com.cappielloantonio.play.databinding.FragmentHomeTabRadioBinding;
+import com.cappielloantonio.play.service.MediaService;
+import com.cappielloantonio.play.ui.activity.MainActivity;
+import com.cappielloantonio.play.viewmodel.HomeViewModel;
+import com.google.common.util.concurrent.ListenableFuture;
+
+@UnstableApi
+public class HomeTabRadioFragment extends Fragment {
+    private static final String TAG = "HomeTabRadioFragment";
+
+    private FragmentHomeTabRadioBinding bind;
+    private MainActivity activity;
+    private HomeViewModel homeViewModel;
+
+    private ListenableFuture<MediaBrowser> mediaBrowserListenableFuture;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        activity = (MainActivity) getActivity();
+
+        bind = FragmentHomeTabRadioBinding.inflate(inflater, container, false);
+        View view = bind.getRoot();
+        homeViewModel = new ViewModelProvider(requireActivity()).get(HomeViewModel.class);
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        initializeMediaBrowser();
+    }
+
+    @Override
+    public void onStop() {
+        releaseMediaBrowser();
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        bind = null;
+    }
+
+    private void initializeMediaBrowser() {
+        mediaBrowserListenableFuture = new MediaBrowser.Builder(requireContext(), new SessionToken(requireContext(), new ComponentName(requireContext(), MediaService.class))).buildAsync();
+    }
+
+    private void releaseMediaBrowser() {
+        MediaBrowser.releaseFuture(mediaBrowserListenableFuture);
+    }
+}

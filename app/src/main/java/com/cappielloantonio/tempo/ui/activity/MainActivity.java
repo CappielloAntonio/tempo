d09e7e7981e1cec0f7c5acbce848795feab5e7d3
antonio <antoniocappiello.play@gmail.com>
2023-06-24 13:38:21 +0200

    fix: collapse player bottom sheet on logout

--- app/src/main/java/com/cappielloantonio/tempo/ui/activity/MainActivity.java
+++ app/src/main/java/com/cappielloantonio/tempo/ui/activity/MainActivity.java
@@ -247,6 +247,7 @@ public class MainActivity extends BaseActivity {
     }
 
     private void goToLogin() {
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
         setBottomNavigationBarVisibility(false);
         setBottomSheetVisibility(false);
 

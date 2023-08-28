723bdf97712f7f9e24a9a39d603f1da463a97d01
antonio <antoniocappiello.play@gmail.com>
2023-08-28 12:26:04 +0200

    clean: code cleanup

--- app/src/main/java/com/cappielloantonio/tempo/ui/adapter/SongHorizontalAdapter.java
+++ app/src/main/java/com/cappielloantonio/tempo/ui/adapter/SongHorizontalAdapter.java
@@ -33,8 +33,8 @@ public class SongHorizontalAdapter extends RecyclerView.Adapter<SongHorizontalAd
     public SongHorizontalAdapter(ClickCallback click, boolean showCoverArt, boolean showAlbum) {
         this.click = click;
         this.showCoverArt = showCoverArt;
-        this.songs = Collections.emptyList();
         this.showAlbum = showAlbum;
+        this.songs = Collections.emptyList();
     }
 
     @NonNull

678c61e569bd73a17f9495376a5689cd513c44c2
antonio <antoniocappiello.play@gmail.com>
2023-10-12 22:34:48 +0200

    clean: removed unused properties of podcast episodes

--- app/src/main/java/com/cappielloantonio/tempo/subsonic/models/PodcastEpisode.kt
+++ app/src/main/java/com/cappielloantonio/tempo/subsonic/models/PodcastEpisode.kt
@@ -12,12 +12,10 @@ class PodcastEpisode : Parcelable {
     var id: String? = null
     @SerializedName("parent")
     var parentId: String? = null
-    @SerializedName("isDir")
     var isDir = false
     var title: String? = null
     var album: String? = null
     var artist: String? = null
-    var track: Int? = null
     var year: Int? = null
     var genre: String? = null
     @SerializedName("coverArt")
@@ -25,26 +23,14 @@ class PodcastEpisode : Parcelable {
     var size: Long? = null
     var contentType: String? = null
     var suffix: String? = null
-    var transcodedContentType: String? = null
-    var transcodedSuffix: String? = null
     var duration: Int? = null
     @SerializedName("bitRate")
     var bitrate: Int? = null
     var path: String? = null
-    @SerializedName("isVideo")
     var isVideo: Boolean = false
-    var userRating: Int? = null
-    var averageRating: Double? = null
-    var playCount: Long? = null
-    var discNumber: Int? = null
     var created: Date? = null
-    var starred: Date? = null
-    var albumId: String? = null
     var artistId: String? = null
     var type: String? = null
-    var bookmarkPosition: Long? = null
-    var originalWidth: Int? = null
-    var originalHeight: Int? = null
     var streamId: String? = null
     var channelId: String? = null
     var description: String? = null

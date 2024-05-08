f6b430d00747f8b66d8851b3003a03d561ca240f
PeanutMelonSeedBigAlmond <PeanutMelonSeedBigAlmond@outlook.com>
2024-05-08 17:40:19 +0800

    feat: better ignore battery optimizations ux
    confirm battery optimization by click system dialog instead of choose app manually.

--- app/src/main/java/com/cappielloantonio/tempo/ui/dialog/BatteryOptimizationDialog.java
+++ app/src/main/java/com/cappielloantonio/tempo/ui/dialog/BatteryOptimizationDialog.java
@@ -1,15 +1,20 @@
 package com.cappielloantonio.tempo.ui.dialog;
 
+import android.annotation.SuppressLint;
 import android.app.Dialog;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.OptIn;
 import androidx.fragment.app.DialogFragment;
 import androidx.media3.common.util.UnstableApi;
 
+import com.cappielloantonio.tempo.App;
 import com.cappielloantonio.tempo.R;
 import com.cappielloantonio.tempo.databinding.DialogBatteryOptimizationBinding;
 import com.cappielloantonio.tempo.util.Preferences;
@@ -17,7 +22,6 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 @OptIn(markerClass = UnstableApi.class)
 public class BatteryOptimizationDialog extends DialogFragment {
-
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -32,10 +36,17 @@ public class BatteryOptimizationDialog extends DialogFragment {
                 .create();
     }
 
+    @SuppressLint("BatteryLife")
     private void openPowerSettings() {
-        Intent intent = new Intent();
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.setAction(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS);
-        startActivity(intent);
+        try {
+            Intent intent = new Intent();
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+            intent.setData(Uri.parse("package:"+App.getContext().getPackageName()));
+            startActivity(intent);
+        }catch (ActivityNotFoundException e){
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
+        }
     }
 }
